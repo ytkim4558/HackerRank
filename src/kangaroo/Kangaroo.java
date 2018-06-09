ee161c763e3c8e5acbda463121669f48e4c731cf
ytkim4558 <ytkim4558@naver.com>
2018-06-10 08:58:25 +0900

    캥거루 문제 품

---
+++ src/kangaroo/Kangaroo.java
@@ -0,0 +1,59 @@
+package kangaroo;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Kangaroo {
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        double bunja = x1-x2;
+        double bunmo = v2-v1;
+        String result;
+        if(bunmo != 0) {
+            if(bunmo * bunja > 0) {
+                double t = bunja/bunmo;
+                if(t == (int)t) {
+                    result = "YES";
+                } else {
+                    result = "NO";
+                }
+            }
+            else result = "NO";
+        } else {
+            if(bunja == 0) result = "YES";
+            else {
+                result = "NO";
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
